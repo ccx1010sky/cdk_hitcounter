2dfa7d22079d4cbbc9cff81fd5f865c8b3c78405
ccx1010sky <ccx1010@gmail.com>
2024-03-02 14:22:46 +0000

    defines an AWS Lambda resource

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,19 +1,16 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
-import { Construct } from 'constructs';
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Code, Function, Runtime } from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'CdkWorkshopQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    // defines an AWS Lambda resource
+    const hello = new Function(this, "HelloHandler", {
+      runtime: Runtime.NODEJS_18_X, // execution environment
+      code: Code.fromAsset("lambda"), // code loaded from "lambda" directory
+      handler: "hello.handler", // file is "hello", function is "handler"
     });
-
-    const topic = new sns.Topic(this, 'CdkWorkshopTopic');
-
-    topic.addSubscription(new subs.SqsSubscription(queue));
   }
 }
